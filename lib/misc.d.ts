c78d30f855a6fd63dc426eef5f14b49334077a64
James Hsu <jameshsu1125@gmail.com>
2025-08-07 13:26:15 +0800

    feat: add Vite configuration and TypeScript build setup
    - Created vite.config.js and vite.config.ts for Vite configuration.
    - Added support for CSS preprocessing with Less and defined global variables.
    - Configured build settings for library output in ES and CommonJS formats.
    - Included plugins for CSS injection and TypeScript declaration generation.
    - Set up asset inclusion for various file types.
    - Configured development server to open automatically on port 5173.
    - Added tsconfig.tsbuildinfo for TypeScript build information.

--- lib/misc.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const GET_DEVICE: () => "mobile" | "desktop";
-export declare const CHECK_PARENT_HAS_CLASS: (e: TouchEvent | MouseEvent, classDataset: string[]) => boolean;
-export declare const FIND_ROOT: (e: TouchEvent | MouseEvent) => HTMLElement | undefined;
--- src/docs/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  name: 'lesca-click',
-  description: 'use for control preventDefault of touch event',
-};
